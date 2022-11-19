b6d17aaeaf3c88a4fe30d65155a6ef24030acf87
Amallyn <amallyn@pm.me>
2022-11-19 07:31:03 +0100

    player plays x turns demo

--- examples/ten_levels_01/10_levels_01.py
+++ examples/ten_levels_01/10_levels_01.py
@@ -32,7 +32,7 @@ levels_difficulty = {
     "2": 2,
     "3": 3,
     "4": 5,
-    "5": 7,
+    "5": 14,
     "6": 10,
     "7": 14,
     "8": 20,
@@ -60,8 +60,8 @@ class Player:
         Init
         """
         self.name = name
-        experience = 0
-        cleared_levels = 0
+        self.experience = 0
+        self.cleared_levels = 0
 
     def reset(self):
         """
@@ -71,6 +71,13 @@ class Player:
         # win ratio = (1 - difficulty/100) * experience/100
         self.experience = 0
         self.cleared_levels = 0
+    
+    def clear_level(self):
+        """
+        player clears a new level
+        """
+        self.cleared_levels +=1
+
 
 class GameEngine:
     """
@@ -84,10 +91,16 @@ class GameEngine:
         """
         pass
     def play_level(self, player, level):
+        """
+        Player plays one level
+        returns win or lose Boolean
+        """
         r = random()
         difficulty = float(0.01* levels_difficulty[str(level)])
         win = r >= difficulty
         print("| level {:2d}".format(level), "| difficulty", "{:.2f}".format(difficulty), "| result: ", "{:.3f}".format(r), "| win: ", win, " |")
+        if win:
+            player.clear_level()        
         return win
 
 class Simulator:
@@ -95,16 +108,21 @@ class Simulator:
     Simulator
     simulate a player playing x turns
     """
-    def __init__(self):
+    def __init__(self, game_engine = GameEngine()):
         """
         Init
         """
-        pass
+        self.game_engine = game_engine
 
     def run(self, player, turns):
         """
         runs a simulation
         """
+        pass
+
+    def play_x_turns(self, player, turns):
+        for i in range(0, turns):
+            self.game_engine.play_level(player, 1 + player.cleared_levels)
 
 class Calculator:
     """
@@ -153,10 +171,21 @@ def main(argv=None):
 
     player = Player("player one")
     game_engine = GameEngine()
-    print("== ", player.name, " ==")
+    print("== ", player.name, " plays levels 1 to 10 ==")
+    # player plays levels 1 to 10
     for level in range(1, 11):
         game_engine.play_level(player, level)
-        
+
+    simulator = Simulator(game_engine)
+    player.reset()
+    print()
+    print("== ", player.name, " plays levels x turns ==")
+    # player plays x turns while needing to unlock levels
+    turns = 10
+    simulator.play_x_turns(player, turns)
+    print()
+    print(player.name, "cleared", player.cleared_levels, "levels in", turns, "turns.")
+
 if __name__ == "__main__":
     import sys
     print("\n" + __doc__ + "\n" + __copyright__ + "\n" + __license__ +"\n" )
